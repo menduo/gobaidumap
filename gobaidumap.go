43b7cb92acc564d9436ed2534f6ff358d3dd314e
shajiquan <shajiquan@gmail.com>
2016-01-04 12:49:12 +0800

    添加注释

--- gobaidumap.go
+++ gobaidumap.go
@@ -1,11 +1,11 @@
-package baidumap
+package gobaidumap
 
 import (
-    "encoding/json"
-    "errors"
-    "fmt"
-    "io/ioutil"
-    "net/http"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 // 百度地图API GEO ： http://developer.baidu.com/map/webservice-geocoding.htm
@@ -15,121 +15,127 @@ import (
 // 注意，请到百度地图开发者中心申请自己的 App Key，下方的 key 是百度提供的，不保证永远有效。
 
 const (
-    AppKey       string = "F454f8a5efe5e577997931cc01de3974" // baidu's
-    reqURLForGEO string = "http://api.map.baidu.com/geocoder/v2/?ak="
-    reqURLForIP  string = "http://api.map.baidu.com/location/ip?ak="
+	// AppKey 百度地图 AppKey
+	AppKey string = "F454f8a5efe5e577997931cc01de3974" // baidu's
+
+	// reqURLForGEO API URL for GetAddressViaGEO 通过 GEO 坐标信息获取地址
+	reqURLForGEO string = "http://api.map.baidu.com/geocoder/v2/?ak="
+
+	// reqURLForIP API URL for GetAddressViaIP 通过 IP 获取地址
+	reqURLForIP string = "http://api.map.baidu.com/location/ip?ak="
 )
 
-var (
-    httpClient = http.Client{}
-)
-
+// GetAddressViaGEO 通过 GEO 坐标信息获取地址
 func GetAddressViaGEO(lat, lng string) (*StructGEOToAddress, error) {
-    res := new(StructGEOToAddress)
+	res := new(StructGEOToAddress)
 
-    parameter := fmt.Sprintf("&location=%s,%s&output=json&pois=0", lat, lng)
-    reqURL := fmt.Sprintf("%s%s%s", reqURLForGEO, AppKey, parameter)
+	parameter := fmt.Sprintf("&location=%s,%s&output=json&pois=0", lat, lng)
+	reqURL := fmt.Sprintf("%s%s%s", reqURLForGEO, AppKey, parameter)
 
-    res2, err := requestBaidu("GetAddressViaGEO", reqURL)
-    if err != nil {
-        return res, err
-    }
+	res2, err := requestBaidu("GetAddressViaGEO", reqURL)
+	if err != nil {
+		return res, err
+	}
 
-    if res2.(*StructGEOToAddress).Status != 0 {
-        message := fmt.Sprintf("百度 API 报错：%s", res2.(*StructGEOToAddress).Msg)
-        return res, errors.New(message)
-    }
+	if res2.(*StructGEOToAddress).Status != 0 {
+		message := fmt.Sprintf("百度 API 报错：%s", res2.(*StructGEOToAddress).Msg)
+		return res, errors.New(message)
+	}
 
-    res3 := res2.(*StructGEOToAddress)
-    return res3, nil
+	res3 := res2.(*StructGEOToAddress)
+	return res3, nil
 }
 
+// GetGeoViaAddress 通过地址获得 GEO 坐标
 func GetGeoViaAddress(address string) (*StructAddressToGEO, error) {
-    res := new(StructAddressToGEO)
+	res := new(StructAddressToGEO)
 
-    parameter := fmt.Sprintf("&address=%s&output=json&pois=1", address)
-    reqURL := fmt.Sprintf("%s%s%s", reqURLForGEO, AppKey, parameter)
+	parameter := fmt.Sprintf("&address=%s&output=json&pois=1", address)
+	reqURL := fmt.Sprintf("%s%s%s", reqURLForGEO, AppKey, parameter)
 
-    res2, err := requestBaidu("GetGeoViaAddress", reqURL)
-    if err != nil {
-        return res, err
-    }
+	res2, err := requestBaidu("GetGeoViaAddress", reqURL)
+	if err != nil {
+		return res, err
+	}
 
-    if res2.(*StructAddressToGEO).Status != 0 {
-        message := fmt.Sprintf("百度 API 报错：%s", res2.(*StructAddressToGEO).Msg)
-        return res, errors.New(message)
-    }
+	if res2.(*StructAddressToGEO).Status != 0 {
+		message := fmt.Sprintf("百度 API 报错：%s", res2.(*StructAddressToGEO).Msg)
+		return res, errors.New(message)
+	}
 
-    res3 := res2.(*StructAddressToGEO)
-    return res3, nil
+	res3 := res2.(*StructAddressToGEO)
+	return res3, nil
 }
 
+// GetAddressViaIP 通过 IP 获取地址
 func GetAddressViaIP(address string) (*StructIPToAddress, error) {
-    res := new(StructIPToAddress)
+	res := new(StructIPToAddress)
 
-    parameter := fmt.Sprintf("&ip=%s&output=json&pois=0", address)
-    reqURL := fmt.Sprintf("%s%s%s", reqURLForIP, AppKey, parameter)
+	parameter := fmt.Sprintf("&ip=%s&output=json&pois=0", address)
+	reqURL := fmt.Sprintf("%s%s%s", reqURLForIP, AppKey, parameter)
 
-    res2, err := requestBaidu("GetAddressViaIP", reqURL)
-    if err != nil {
-        return res, err
-    }
+	res2, err := requestBaidu("GetAddressViaIP", reqURL)
+	if err != nil {
+		return res, err
+	}
 
-    if res2.(*StructIPToAddress).Status != 0 {
-        message := fmt.Sprintf("百度 API 报错：%s", res2.(*StructIPToAddress).Message)
-        return res, errors.New(message)
-    }
+	if res2.(*StructIPToAddress).Status != 0 {
+		message := fmt.Sprintf("百度 API 报错：%s", res2.(*StructIPToAddress).Message)
+		return res, errors.New(message)
+	}
 
-    res3 := res2.(*StructIPToAddress)
-    return res3, nil
+	res3 := res2.(*StructIPToAddress)
+	return res3, nil
 
 }
 
+// requestBaidu 构建 HTTP 请求
 func requestBaidu(reqType, reqURL string) (interface{}, error) {
 
-    res, err := getResStruct(reqType)
-    if err != nil {
-        return res, err
-    }
+	res, err := getResStruct(reqType)
+	if err != nil {
+		return res, err
+	}
+	httpClient := http.Client{}
+	resp, err := httpClient.Get(reqURL)
 
-    resp, err := httpClient.Get(reqURL)
+	if err != nil {
+		return res, err
+	}
 
-    if err != nil {
-        return res, err
-    }
+	defer resp.Body.Close()
 
-    defer resp.Body.Close()
+	bytes, _ := ioutil.ReadAll(resp.Body)
 
-    bytes, _ := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode == 200 {
 
-    if resp.StatusCode == 200 {
+		err := json.Unmarshal(bytes, &res)
 
-        err := json.Unmarshal(bytes, &res)
+		if err != nil {
+			return res, err
+		}
 
-        if err != nil {
-            return res, err
-        }
+	} else {
+		return res, errors.New("请求百度API失败，状态码不等于200")
+	}
 
-    } else {
-        return res, errors.New("请求百度API失败，状态码不等于200")
-    }
-
-    return res, nil
+	return res, nil
 }
 
+// getResStruct 处理百度 API 返回数据，映射到结构体中
 func getResStruct(reqType string) (interface{}, error) {
-    var res interface{}
+	var res interface{}
 
-    if reqType == "GetAddressViaIP" {
-        return new(StructIPToAddress), nil
-    }
+	if reqType == "GetAddressViaIP" {
+		return new(StructIPToAddress), nil
+	}
 
-    if reqType == "GetGeoViaAddress" {
-        return new(StructAddressToGEO), nil
-    }
+	if reqType == "GetGeoViaAddress" {
+		return new(StructAddressToGEO), nil
+	}
 
-    if reqType == "GetAddressViaGEO" {
-        return new(StructGEOToAddress), nil
-    }
-    return res, errors.New("结构体请求错误")
+	if reqType == "GetAddressViaGEO" {
+		return new(StructGEOToAddress), nil
+	}
+	return res, errors.New("结构体请求错误")
 }
